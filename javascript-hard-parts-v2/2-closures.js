1d0640e641ca5397026392ee949fa2341dfd13ee
Carl Aiau <carl@ortus.co.nz>
2020-01-28 19:50:56 +1300

    Closure work exercises completed

--- javascript-hard-parts-v2/2-closures.js
+++ javascript-hard-parts-v2/2-closures.js
@@ -1,28 +1,26 @@
 // http://csbin.io/closures
 
-// Type JavaScript here and click "Run Code" or press Ctrl + s
-console.log('Hello, world!');
-
 
 function createFunction() {
-
+  return () => {
+    console.log("Hi")
+  }
+  
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const function1 = createFunction();
-// function1();
+const function1 = createFunction();
+function1();
 
 
+createFunctionPrinter = (input) => () => {console.log(input) }
 
-function createFunctionPrinter(input) {
-
-}
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const printSample = createFunctionPrinter('sample');
-// printSample();
-// const printHello = createFunctionPrinter('hello');
-// printHello();
+const printSample = createFunctionPrinter('sample');
+printSample();
+const printHello = createFunctionPrinter('hello');
+printHello();
 
 
 
@@ -41,25 +39,35 @@ const jasCounter = outer();
 // Uncomment each of these lines one by one.
 // Before your do, guess what will be logged from each function call.
 
-// willCounter();
-// willCounter();
-// willCounter();
+willCounter(); // Will output 1
+willCounter(); // will output 2
+willCounter(); // will output 3
 
-// jasCounter();
-// willCounter();
-
+jasCounter(); // will output 1
+willCounter(); // will output 4
 
 
 function addByX(x) {
-
+  return function(y){
+    return x + y
+  }
 }
+// Lol ES6, Readability?!
+addByX = (x) => (y) => x + y
 
 const addByTwo = addByX(2);
-
 // now call addByTwo with an input of 1
+console.log(addByTwo(1)) //should return 3
+console.log(addByTwo(2)) //should return 4
+console.log(addByTwo(3)) //should return 5
 
+const addByThree = addByX(3)
+console.log(addByThree(1)) //should return 4
+console.log(addByThree(2)) //should return 5
 
-// now call addByTwo with an input of 2
+const addByFour = addByX(4)
+console.log(addByFour(4)) //should return 8
+console.log(addByFour(10)) //should return 14
 
 
 
@@ -67,43 +75,87 @@ const addByTwo = addByX(2);
 // Extension
 //--------------------------------------------------
 
-function once(func) {
-
+/* Longer version */
+function once(func){
+  let value = null
+  function inner(input){
+    if(!value) value = func(input)
+    return value
+  }
+  return inner
 }
+
+function once(func) {
+  let value = null
+  return (input) => {
+    if(!value) value = func(input)
+    return value
+  }
+}
+
+
 
 const onceFunc = once(addByTwo);
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// console.log(onceFunc(4));  //should log 6
-// console.log(onceFunc(10));  //should log 6
-// console.log(onceFunc(9001));  //should log 6
+console.log(onceFunc(4));  //should log 6
+console.log(onceFunc(10));  //should log 6
+console.log(onceFunc(9001));  //should log 6
 
-
+/* Longer version */
 function after(count, func) {
-
+  let counter = 0;
+  function inner(){
+    counter++
+    if(count <= counter){
+      func()
+    }  
+  }
+  return inner
 }
+
+function after(count, func){
+  let counter = 0
+  return () => {
+    counter++
+    if(count <= counter) func()
+  }
+}
+
+
 
 const called = function() { console.log('hello') };
 const afterCalled = after(3, called);
 
-// afterCalled(); // -> nothing is printed
-// afterCalled(); // -> nothing is printed
-// afterCalled(); // -> 'hello' is printed
+afterCalled(); // -> nothing is printed
+afterCalled(); // -> nothing is printed
+afterCalled(); // -> 'hello' is printed
 
-
+// not sure about this extension definition?
 function delay(func, wait) {
+  setTimeout(func, wait)
+} 
 
-}
-
+delay(
+  function(){ 
+  console.log("Hello after a ")}, 
+  200
+)
 
 function rollCall(names) {
-
+  let i = 0
+  return () => {
+    if(i < names.length) console.log(names[i])
+    else console.log("Everyone Accounted For")
+    i++
+  }
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
-// rollCaller() // -> Should log 'Victoria'
-// rollCaller() // -> Should log 'Juan'
-// rollCaller() // -> Should log 'Ruth'
-// rollCaller() // -> Should log 'Everyone accounted for'
+console.log("\nChallenge 8")
+const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
+rollCaller() // -> Should log 'Victoria'
+rollCaller() // -> Should log 'Juan'
+rollCaller() // -> Should log 'Ruth'
+rollCaller() // -> Should log 'Everyone accounted for'
 
