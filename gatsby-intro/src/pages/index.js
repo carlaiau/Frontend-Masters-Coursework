9fbca98872f9e494ab5897051dc704e4a0921d18
Carl Aiau <carl@ortus.co.nz>
2020-03-03 21:03:54 +1300

    Blog running with MDX ad graphql oh yeah

--- gatsby-intro/src/pages/index.js
+++ gatsby-intro/src/pages/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import PostPreview from '../components/post-preview'
+import usePosts from '../hooks/use-posts'
 
-export default () => (
+export default () => {
+    const posts = usePosts();
+
+    return (
     <Layout>
         <h1>Ello Frontend Masters!</h1>
+        {posts.map(post => (
+            <PostPreview key={post.slug} post={post}/>
+        ))}
     </Layout>
-)
+    )
+}
