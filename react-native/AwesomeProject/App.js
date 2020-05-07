4a32e3dd6a27ada0282d8870788403308b9808c2
Carl Aiau <carl@ortus.co.nz>
2020-05-07 12:43:18 +1200

    Style Exercise 1

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -2,24 +2,51 @@ import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
+  const listItems = [
+    { name: 'Cyan', hex: '#2aa198' },
+    { name: 'Blue', hex: '#268bd2' },
+    { name: 'Magenta', hex: '#d33682' },
+    { name: 'Orange', hex: '#cb4b16' },
+  ];
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={[styles.container, styles.pink]}>
-        <Text style={{ color: '#fff' }}>Hello, world!</Text>
+      <View style={styles.container}>
+        <Text style={[styles.text, { marginBottom: 5 }]}>
+          Here are some boxes of different colours
+        </Text>
+        {listItems.map((li, i) => (
+          <View style={[styles.listItem, { backgroundColor: li.hex }]} key={i}>
+            <Text style={[styles.text, styles.listItemText]}>
+              {li.name} {li.hex}
+            </Text>
+          </View>
+        ))}
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  pink: {
-    backgroundColor: 'pink',
-  },
   container: {
-
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 3,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  listItemText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
   },
 });
 
