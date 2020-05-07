f96b7fe67a2517187f6d95f7ae07fb3d7e8fd1db
Carl Aiau <carl@ortus.co.nz>
2020-05-07 14:56:45 +1200

    Solution using FlatList and Global COLORS_PALETTE

--- react-native/AwesomeProject/screens/ColorPalette.js
+++ react-native/AwesomeProject/screens/ColorPalette.js
@@ -3,38 +3,17 @@ import { Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 
 import ColorBox from '../components/ColorBox';
 
-const ColorPalette = () => {
-  const COLORS = [
-    { name: 'Base03', hex: '#002b36' },
-    { name: 'Base02', hex: '#073642' },
-    { name: 'Base01', hex: '#586e75' },
-    { name: 'Base00', hex: '#657b83' },
-    { name: 'Base0', hex: '#839496' },
-    { name: 'Base1', hex: '#93a1a1' },
-    { name: 'Base2', hex: '#eee8d5' },
-    { name: 'Base3', hex: '#fdf6e3' },
-    { name: 'Yellow', hex: '#b58900' },
-    { name: 'Orange', hex: '#cb4b16' },
-    { name: 'Red', hex: '#dc322f' },
-    { name: 'Magenta', hex: '#d33682' },
-    { name: 'Violet', hex: '#6c71c4' },
-    { name: 'Blue', hex: '#268bd2' },
-    { name: 'Cyan', hex: '#2aa198' },
-    { name: 'Green', hex: '#859900' },
-  ];
+const ColorPalette = ({ route }) => {
+  const { colors } = route.params;
 
   return (
     <SafeAreaView style={styles.everything}>
       <FlatList
         style={styles.container}
-        ListHeaderComponent={
-          <Text style={styles.heading}>Solarized Color Palette</Text>
-        }
-        data={COLORS}
+        data={colors}
         renderItem={({ item }) => <ColorBox name={item.name} hex={item.hex} />}
-        keyExtractor={(color, i) => i}
+        keyExtractor={(_, i) => i}
       />
-      <Text>Hello</Text>
     </SafeAreaView>
   );
 };
@@ -45,13 +24,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingHorizontal: 10,
-    paddingVertical: 50,
-  },
-  heading: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginBottom: 10,
+    padding: 10,
+
   },
 });
 
--- react-native/AwesomeProject/screens/Home.js
+++ react-native/AwesomeProject/screens/Home.js
@@ -1,25 +1,83 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const Home = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity onPress={() => navigation.push('ColorPalette')}>
-                <Text style={styles.heading}>Solarized</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  const colors = [
+    {
+      name: 'Solarized',
+      colors: [
+        { name: 'Base03', hex: '#002b36' },
+        { name: 'Base02', hex: '#073642' },
+        { name: 'Base01', hex: '#586e75' },
+        { name: 'Base00', hex: '#657b83' },
+        { name: 'Base0', hex: '#839496' },
+        { name: 'Base1', hex: '#93a1a1' },
+        { name: 'Base2', hex: '#eee8d5' },
+        { name: 'Base3', hex: '#fdf6e3' },
+        { name: 'Yellow', hex: '#b58900' },
+        { name: 'Orange', hex: '#cb4b16' },
+        { name: 'Red', hex: '#dc322f' },
+        { name: 'Magenta', hex: '#d33682' },
+        { name: 'Violet', hex: '#6c71c4' },
+        { name: 'Blue', hex: '#268bd2' },
+        { name: 'Cyan', hex: '#2aa198' },
+        { name: 'Green', hex: '#859900' },
+      ]
+    },
+    {
+      name: 'Rainbow',
+      colors: [
+        { name: 'Red', hex: '#FF0000' },
+        { name: 'Orange', hex: '#FF7F00' },
+        { name: 'Yellow', hex: '#FFFF00' },
+        { name: 'Green', hex: '#00FF00' },
+        { name: 'Violet', hex: '#8B00FF' },
+      ]
+    },
+    {
+      name: 'Frontend Masters',
+      colors: [
+        { name: 'Red', hex: '#c02d28' },
+        { name: 'Black', hex: '#3e3e3e' },
+        { name: 'Grey', hex: '#8a8a8a' },
+        { name: 'White', hex: '#ffffff' },
+        { name: 'Orange', hex: '#e66225' },
+      ]
+    }
+  ]
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.container}
+        data={colors}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              const { name, colors } = item
+              navigation.push('ColorPalette', { name, colors })
+            }}
+          >
+            <Text style={styles.heading}>{item.name}</Text>
+          </TouchableOpacity>)}
+        keyExtractor={(_, i) => i}
+      />
+
+
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10
-    },
-    heading: {
-        fontSize: 18,
-        fontWeight: '700'
-    }
-})
+  container: {
+    padding: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginBottom: 10
+  },
+});
 
 export default Home;
